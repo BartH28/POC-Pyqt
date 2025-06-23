44e6fdee3d91e5649c6366099216d824a0333803
BartH28 <henriquelb28@gmail.com>
2025-06-23 18:34:45 -0300

    Refactor with threads and events.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import keyboard
 from PyQt6.QtWidgets import QApplication,QSystemTrayIcon, QMessageBox, QMenu, QWidget, QVBoxLayout, QPushButton, QLabel, QTextEdit
 from PyQt6.QtGui import QPixmap, QIcon, QAction, QFont
-from PyQt6.QtCore import Qt, QObject, QThread, pyqtSignal
+from PyQt6.QtCore import Qt, QObject, QThread, pyqtSignal, QEvent
 from tkinter import Tk
 from screencapture import ScreenCapture
 from PIL import Image
@@ -13,40 +13,6 @@ import xml.etree.ElementTree as ET
 
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_PATH,"database","kanjidic_full.db")
-
-class DatabaseWorker(QObject):
-    resultado_pronto = pyqtSignal(list)  # lista de dicionários com kanji
-
-    def __init__(self, ocr_string):
-        super().__init__()
-        self.ocr_string = ocr_string
-
-    def extract_kanjis(self, string):
-        return ''.join(c for c in string if '\u4e00' <= c <= '\u9fff')
-
-    def run(self):
-        result_list = []
-        kanjis = self.extract_kanjis(self.ocr_string)
-        splited_kanjis = list(kanjis)
-        try:
-            conn = sqlite3.connect(DB_PATH)
-            cursor = conn.cursor()
-            for k in splited_kanjis:
-                cursor.execute("SELECT * FROM kanjidic WHERE kanji = ?", (k,))
-                res = cursor.fetchone()
-                if res:
-                    result_list.append({
-                        "kanji": res[0],
-                        "onyomi": res[1],
-                        "kunyomi": res[2],
-                        "significado": res[3],
-                        "grade": res[4],
-                        "jlpt": res[5]
-                    })
-            conn.close()
-        except Exception as e:
-            print("Erro no worker:", e)
-        self.resultado_pronto.emit(result_list)
 
 
 # Caminho da pasta onde está o tesseract.exe
@@ -63,11 +29,54 @@ def get_tesseract_path():
 pytesseract.pytesseract.tesseract_cmd = get_tesseract_path()
 
 
+class CustomHotkeyEvent(QEvent):
+    def __init__(self, tipo):
+        super().__init__(QEvent.Type(QEvent.registerEventType()))
+        self.tipo = tipo
+
+class DatabaseWorker(QObject):
+    resultado_pronto = pyqtSignal(list)  # lista de dicionários com kanji
+
+    def __init__(self, ocr_string):
+        super().__init__()
+        self.ocr_string = ocr_string
+        
+
+    def extract_kanjis(self, string):
+        return ''.join(c for c in string if '\u4e00' <= c <= '\u9fff')
+
+    def run(self):
+        result_list = []
+        print(self.ocr_string)
+        kanjis = self.extract_kanjis(self.ocr_string)
+        splited_kanjis = list(kanjis)
+
+        try:
+            with sqlite3.connect(DB_PATH) as conn:
+                cursor = conn.cursor()
+                for k in splited_kanjis:
+                    cursor.execute("SELECT * FROM kanjidic WHERE kanji = ?", (k,))
+                    res = cursor.fetchone()
+                    if res:
+                        result_list.append({
+                            "kanji": res[0],
+                            "onyomi": res[1],
+                            "kunyomi": res[2],
+                            "significado": res[3],
+                            "grade": res[4],
+                            "jlpt": res[5]
+                        })
+        except Exception as e:
+            print("Erro no worker:", e)
+        
+        self.resultado_pronto.emit(result_list)
+
 class MeuApp(QWidget):
     def __init__(self):
         super().__init__()
               
         self.setWindowTitle("Test PyQT")
+        self.threads_ativas = []
         
         screen_geometry = QApplication.primaryScreen().geometry()
         screen_width = screen_geometry.width()
@@ -118,6 +127,18 @@ class MeuApp(QWidget):
         # Configurar o layout na janela
         self.setLayout(layout)
         
+    def event(self, event):
+        if isinstance(event, CustomHotkeyEvent):
+            if event.tipo == "capture":
+                self.capture_button()
+            elif event.tipo == "toggle":
+                self.toggle_visibility()
+            elif event.tipo == "exit":
+                self.quit_app()
+            return True
+        return super().event(event)
+
+
     def config_tray(self):
         #Tray Config
         self.tray_icon = QSystemTrayIcon(self)
@@ -141,9 +162,9 @@ class MeuApp(QWidget):
         self.tree = ET.parse('usersettings.xml', None)
         self.root = self.tree.getroot()
 
-        keyboard.add_hotkey(self.tree.find('capture').get('key'), self.capture_button)
-        keyboard.add_hotkey(self.tree.find('toggle_visibility').get('key'), self.toggle_visibility)
-        keyboard.add_hotkey(self.tree.find('exit').get('key'), self.quit_app)
+        keyboard.add_hotkey(self.tree.find('capture').get('key'), lambda: QApplication.postEvent(self, CustomHotkeyEvent("capture")))
+        keyboard.add_hotkey(self.tree.find('toggle_visibility').get('key'), lambda: QApplication.postEvent(self, CustomHotkeyEvent("toggle")))
+        keyboard.add_hotkey(self.tree.find('exit').get('key'), lambda: QApplication.postEvent(self, CustomHotkeyEvent("exit")))
 
     def capture_button(self):
         """Inicializa a captura de tela, salva a imagem"""   
@@ -152,20 +173,29 @@ class MeuApp(QWidget):
             result_ocr = pytesseract.image_to_string(f'./images/{result}.png', lang='jpn+eng')
             
             # Inicializa thread e worker
-            self.thread = QThread()
+            thread = QThread()
             self.worker = DatabaseWorker(result_ocr)
-            self.worker.moveToThread(self.thread)
+            self.worker.moveToThread(thread)
 
             # Conecta sinal ao método que atualiza a interface
             self.worker.resultado_pronto.connect(self.exibir_resultado)
-            self.thread.started.connect(self.worker.run)
-            self.thread.start()
+            self.worker.resultado_pronto.connect(thread.quit)
+            thread.finished.connect(thread.deleteLater)
+            thread.finished.connect(lambda: self.threads_ativas.remove(thread))
+
+            self.threads_ativas.append(thread)
+
+            thread.started.connect(self.worker.run)
+            thread.start()
 
             pixmap = QPixmap(f'./images/{result}.png').scaled(self.label_resultado.size(), aspectRatioMode=Qt.AspectRatioMode.KeepAspectRatio)
             self.label_resultado.setPixmap(pixmap)
             #self.label_resultado_ocr.setText(result_ocr)
-
-            self.show()
+            
+            if not self.isVisible():
+                self.show()
+                self.raise_()  
+                self.activateWindow()
         except Exception as e:
             print(e)
 
@@ -181,33 +211,6 @@ class MeuApp(QWidget):
             <hr>
             """
         self.resultado_box.setHtml(content)
-
-    # def extract_kanjis(self, string):
-    #     return ''.join(c for c in string if '\u4e00' <= c <= '\u9fff')
-
-    # def find_Kanji(self, ocr_string):
-    #     result_list = []
-
-    #     kanjis = self.extract_kanjis(ocr_string)
-    #     splited_kanjis = list(kanjis)
-    #     for k in splited_kanjis:
-    #         try:
-    #             cursor.execute("SELECT * FROM kanjidic WHERE kanji = ?", (k))
-    #             res = cursor.fetchone()
-    #             if res:
-    #                 result_list.append({
-    #                     "kanji": res[0],
-    #                     "onyomi": res[1],
-    #                     "kunyomi": res[2],
-    #                     "significado": res[3],
-    #                     "grade": res[4],
-    #                     "jlpt": res[5]
-    #                 })
-
-    #         except Exception as e:
-    #             print(e)
-
-    #     return result_list
 
     def show_window(self):
         """Mostra a janela principal."""
@@ -228,6 +231,8 @@ class MeuApp(QWidget):
             self.hide()
         else:
             self.show()
+            self.raise_()  # Garante que apareça em frente às outras janelas
+            self.activateWindow()
 
     def start_sc(self):
         root = Tk()
@@ -235,6 +240,7 @@ class MeuApp(QWidget):
         root.mainloop()
         return app.current_time 
 
+    
 # Inicializar o app
 if __name__ == "__main__":
     app = QApplication(sys.argv)
